1261dbc781cbc0031d7e2ac5da9dd39a32ef5e5a
Namelos <namelos@outlook.com>
2017-07-05 21:24:59 +0800

    add combine reducers

---
+++ src/lib/combineReducers.js
@@ -0,0 +1,11 @@
+export const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers).reduce(
+      (nextState, key) => {
+        nextState[key] = reducers[key](state[key], action)
+        return nextState
+      },
+      {}
+    )
+  }
+}
